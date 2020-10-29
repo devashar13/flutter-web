a2855dfc36188e6eec286999154ab07e684b1851
Dev Ashar <dev.ashar2019@vitstudent.ac.in>
2020-10-29 17:14:54 +0530

    added navbar

--- lib/widgets/navbar_mobile.dart
+++ lib/widgets/navbar_mobile.dart
@@ -8,7 +8,7 @@ class NavBarMobile extends StatefulWidget {
 class _NavBarMobileState extends State<NavBarMobile> {
   @override
   Widget build(BuildContext context) {
-    int selectedValue = 1;
+    int selectedValue;
     return Container(
       color: Color.fromRGBO(28, 36, 67, 1),
       child: new Theme(
@@ -16,24 +16,48 @@ class _NavBarMobileState extends State<NavBarMobile> {
           canvasColor: Color.fromRGBO(28, 36, 67, 1),
         ),
         child: new DropdownButton(
+          hint: Center(
+            child: Text(
+              'Menu',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          elevation: 0,
           isExpanded: true,
           value: selectedValue,
           items: [
             DropdownMenuItem(
-              child: Text("Male"),
+              child: Center(
+                child: Text(
+                  "Home",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
               value: 1,
             ),
             DropdownMenuItem(
-              child: Text("Female"),
+              child: Center(
+                  child: Text("About", style: TextStyle(color: Colors.white))),
               value: 2,
             ),
-            DropdownMenuItem(child: Text("Others"), value: 3),
+            DropdownMenuItem(
+                child: Center(
+                    child:
+                        Text("Event", style: TextStyle(color: Colors.white))),
+                value: 3),
+            DropdownMenuItem(
+                child: Center(
+                    child: Text("Team", style: TextStyle(color: Colors.white))),
+                value: 4),
+            DropdownMenuItem(
+                child: Center(
+                    child: Text("Contact Us",
+                        style: TextStyle(color: Colors.white))),
+                value: 5),
           ],
-          onChanged: (value) {
-            setState(() {
-              selectedValue = value;
-            });
-          },
+          onChanged: (_) {},
         ),
       ),
     );
